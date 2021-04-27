5cdaf27e979cf0b28bebe1080a386652593c675a
chanwira <54656643+chanwira@users.noreply.github.com>
2021-04-27 19:45:15 +0700

    Add files via upload

---
+++ 2_Basic_Programming/Divide.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try 
+            {
+                Console.Write("Enter a number:  "); 
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Enter another number:  ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+
+                Console.WriteLine(num1 / num2);
+            }
+            catch (DivideByZeroException e )
+            {
+                Console.WriteLine(e.Message);
+            }
+            catch(FormatException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+            
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
