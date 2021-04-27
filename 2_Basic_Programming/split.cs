369c11eafcaed9f8395feea592b270f7a042cfb1
chanwira <54656643+chanwira@users.noreply.github.com>
2021-04-27 20:54:58 +0700

    Add files via upload

---
+++ 2_Basic_Programming/split.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace homework
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string str_split_comma = "Hello & Hi, Animal and Birds, Earth and Sky";
+
+                string[] broken_str_comma = str_split_comma.Split(',');
+            }
+            catch (DivideByZeroException e) //case input zero
+            {
+                System.Console.WriteLine(sub_str_comma);
+            }
+            
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
