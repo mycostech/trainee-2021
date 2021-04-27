17579a632152db5fe1974fe417d6be0648d7f81a
yossawadeeh <yossawadee_h@cmu.ac.th>
2021-04-27 22:05:17 +0700

    update final code

---
+++ 2_Basic_Programming/divide-cal.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace divide_cal
+{
+    class Program
+    {
+        static void DivideCalculation()
+        {
+
+            try
+            {
+                int dividend;
+                int divisor;
+                int result;
+
+                Console.Write("Input dividend: ");
+                int.TryParse(Console.ReadLine(), out dividend);
+
+                Console.Write("Input divisor: ");
+                int.TryParse(Console.ReadLine(), out divisor);
+
+                //Divide Calculation
+                result = dividend / divisor;
+                Console.WriteLine("Result = {0}", result);
+            }
+
+            catch(Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("End Program");
+            }
+            
+        }
+        static void Main(string[] args)
+        {
+            DivideCalculation();
+        }
+    }
+}
---
+++ 2_Basic_Programming/split-string.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace split_string
+{
+    class Program
+    {
+        static void SplitString()
+        {
+            try
+            {
+                Console.Write("Input String: ");
+                string str = Console.ReadLine();
+
+                string[] words = str.Split(',');
+
+                //Loop for display the word in array.
+                foreach (var word in words)
+                {
+                    Console.WriteLine(word);
+                }
+            }
+
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            finally
+            {
+                Console.WriteLine("End Program");
+            }
+            
+            
+        }
+        static void Main(string[] args)
+        {
+            SplitString();
+        }
+    }
+}
