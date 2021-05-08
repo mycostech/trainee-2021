b7e67722161774c7fb86450a19c5925e39a350ea
yingstm124 <yingstm@hotmail.com>
2021-05-08 09:18:35 +0700

    api test

---
+++ 7_Unit_Test/ApiTest/AuthCtrlTest.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Threading.Tasks;
+using diaryApp_backend;
+using diaryApp_backend.Controllers;
+using diaryApp_backend.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Xunit;
+
+
+namespace ApiTest
+{
+    public class AuthCtrlTest
+    {
+
+        /// create database
+        public diaryAppContext createTestingDB(string name)
+        {
+            var options = new DbContextOptionsBuilder<diaryAppContext>()
+            .UseInMemoryDatabase(databaseName: name)
+            .Options;
+
+            var dbContext = new diaryAppContext(options);
+            dbContext.Seed();
+
+            return dbContext;
+        }
+
+
+        // login
+        [Fact]
+        public async Task LoginSuccess()
+        {
+
+            var dbContext = createTestingDB(nameof(LoginSuccess));
+            var userServe = new AuthService(dbContext);
+            var controller = new AuthController(userServe);
+
+            var res = await controller.Login("testUser1@test.com", "12345");
+
+            dbContext.Dispose();
+
+            Assert.Equal("testUser1", res.Value.Id);
+        }
+
+        // register
+        [Fact]
+        public async Task RegisterSuccess()
+        {
+
+            var dbContext = createTestingDB(nameof(RegisterSuccess));
+            var userServe = new AuthService(dbContext);
+            var controller = new AuthController(userServe);
+
+            var uid = Guid.NewGuid().ToString();
+            var newUser = new Users()
+            {
+
+                Id = uid,
+                Fname = "userFname",
+                Lname = "userLname",
+                Nickname = "userNickname",
+                Birthdate = new DateTime(1999 - 01 - 11),
+                Email = "userT@test.com",
+                Password = "12345"
+            };
+
+
+            var res = await controller.Register(newUser);
+            dbContext.Dispose();
+
+            Assert.Equal(uid, res.Value.Id);
+
+        }
+
+        [Fact]
+        public async Task RegisterError() {
+
+            var dbContext = createTestingDB(nameof(RegisterError));
+            var userServe = new AuthService(dbContext);
+            var controller = new AuthController(userServe);
+
+            var uid = Guid.NewGuid().ToString();
+            var newUser = new Users()
+            {
+
+                Id = uid,
+                Fname = "userFname",
+                Nickname = "userNickname",
+                Birthdate = new DateTime(1999 - 01 - 11),
+                Email = "userT@test.com",
+                Password = "12345"
+            };
+
+
+            var res = await controller.Register(newUser);
+            dbContext.Dispose();
+
+            //Assert.IsType<BadRequestResult>(res.Result);
+            Assert.Null(res.Result);
+        }
+
+
+
+    }
+
+
+}
---
+++ 7_Unit_Test/ApiTest/DbMock.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Security.Cryptography;
+using diaryApp_backend;
+
+namespace ApiTest
+{
+    public static class DbMock
+    {
+        public static void Seed(this diaryAppContext dbContext)
+        {
+            // add User
+            dbContext.Users.Add(new Users
+            {
+                Id = "testUser1",
+                Fname = "UserFirstname1",
+                Lname = "UserLastname1",
+                Nickname = "UserNickName1",
+                Email = "testUser1@test.com",
+                Password = GetHashpassword("12345")
+
+            });
+
+            dbContext.SaveChangesAsync();
+
+
+            // add Event
+            dbContext.Events.Add(new Events
+            {
+                EventName = "testEvent1",
+                DateTime = new DateTime(2008 - 05 - 01),
+                Memo = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc nec tristique tellus, eget interdum massa. In hac habitasse platea dictumst. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce convallis felis dignissim egestas tristique. Quisque elementum sed diam ac varius. Sed sollicitudin venenatis sodales. Maecenas malesuada metus vitae tellus accumsan, et cursus mauris tincidunt. Suspendisse in nibh luctus, maximus mauris eu, efficitur justo. Morbi eu justo sed quam sodales mollis. Nulla eget suscipit nisl. Nam efficitur vestibulum orci, quis placerat nibh ultricies sit amet. In hac habitasse platea dictumst. Vivamus vel accumsan nisi.",
+                UserId = "testUser1"
+            });
+
+
+            dbContext.SaveChangesAsync();
+
+
+        }
+
+        public static string GetHashpassword(string password)
+        {
+
+            // STEP 1 Create the salt value with a cryptographic PRNG:
+            byte[] salt;
+            new RNGCryptoServiceProvider().GetBytes(salt = new byte[16]);
+
+            // STEP 2 Create the Rfc2898DeriveBytes and get the hash value:
+            var pbkdf2 = new Rfc2898DeriveBytes(password, salt, 100000);
+            byte[] hash = pbkdf2.GetBytes(20);
+
+
+            // STEP 3 Combine the salt and password bytes for later use:
+            byte[] hashBytes = new byte[36];
+            Array.Copy(salt, 0, hashBytes, 0, 16);
+            Array.Copy(hash, 0, hashBytes, 16, 20);
+
+            // STEP 4 Turn the combined salt+hash into a string for storage
+            string savedPasswordHash = Convert.ToBase64String(hashBytes);
+
+            return savedPasswordHash;
+        }
+    }
+    
+
+}
