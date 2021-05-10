5075130ec3251fae9620ea94c3f03d2b30b52bea
Pond <sahapap@mycostech.com>
2021-05-10 16:28:59 +0700

    first init

---
+++ 5_Frontend/react-redux/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import ShowUser from './Component/ShowUser'
+
+function App() {
+  return (
+    <div className="App">
+      <ShowUser/>
+    </div>
+  );
+}
+
+export default App;
+
+
+//https://jsonplaceholder.typicode.com/guide/
