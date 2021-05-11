b98317b4d12a4b8b7bfbbef86f5d9583503efb8e
Sahapap <macintosh@Macintoshs-MacBook-Pro.local>
2021-05-11 21:58:44 +0700

    add redux-logger

--- 5_Frontend/react-redux/src/Store/index.ts
+++ 5_Frontend/react-redux/src/Store/index.ts
@@ -1,6 +1,7 @@
 import { combineReducers, createStore, applyMiddleware } from 'redux'
 import reduxThunk from 'redux-thunk'
 import { UserReducer } from './UserStore'
+import logger from 'redux-logger'
 
 const rootReducer = combineReducers({
     UserReducer
@@ -9,7 +10,7 @@ const rootReducer = combineReducers({
 export type RootState = ReturnType<typeof rootReducer>
 
 export default function configStore(){
-    let store = createStore(rootReducer, {}, applyMiddleware(reduxThunk))
+    let store = createStore(rootReducer, {}, applyMiddleware(logger, reduxThunk))
 
     return store
 }
