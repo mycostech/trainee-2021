f173a715f3d228f9b42c2ccd195d796fb79ad3e7
chanwira <54656643+chanwira@users.noreply.github.com>
2021-05-17 21:11:52 +0700

    Update todoAPI.ts
    edit api

--- 5_Frontend/todolist--frontend/src/api/todoAPI.ts
+++ 5_Frontend/todolist--frontend/src/api/todoAPI.ts
@@ -1,20 +1,19 @@
-
 import axios from 'axios'
 import todo from '../models/todolist'
 
-const API_URL = process.env.REACT_APP_API_URL
+const API_URL = process.env.REACT_APP_API_URL|| 'http://localhost:5000/api/todoitems'
 
 const userApi = {
 
     getUsers: () => {
-        return axios.get<todo[]>(API_URL + 'users')
+        return axios.get<todo[]>(API_URL )
     },
     postUser: (Todo: todo) => {
-        return axios.post<todo>(API_URL + 'posts', {...Todo})
+        return axios.post<todo>(API_URL + '/posts', {...Todo})
         
     },
     deleteUser: (Todo :any) => {
-        return axios.delete<todo>(API_URL + 'Notes/'+Todo)
+        return axios.delete<todo>(API_URL + '/todo/'+Todo)
     }
 }
 
